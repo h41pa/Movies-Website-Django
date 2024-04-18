da081fd41646dba449139c0de084f9c4b408505b
h41pa <madalin_chelu@yahoo.com>
2024-04-18 20:17:10 +0300

    push

--- website/settings.py
+++ website/settings.py
@@ -121,7 +121,7 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CSRF_TRUSTED_ORIGINS = ['*']
