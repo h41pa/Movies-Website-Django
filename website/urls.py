7adaa1d08fa1591906d18df375f32c967acbec60
h41pa <madalin_chelu@yahoo.com>
2024-04-19 15:53:31 +0300

    push

--- website/urls.py
+++ website/urls.py
@@ -25,8 +25,5 @@ urlpatterns = [
     path('', include('netflix.urls'))
 ]
 
-# urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-# urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
