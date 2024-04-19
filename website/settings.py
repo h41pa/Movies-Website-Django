8c0e196b35f1a0760e1020f23fc252bc5d76ee0b
h41pa <madalin_chelu@yahoo.com>
2024-04-19 17:26:52 +0300

    push

--- website/settings.py
+++ website/settings.py
@@ -122,7 +122,7 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
