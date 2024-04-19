3c0871af3d14b4b9b13a7b462f2dc154453459d2
h41pa <madalin_chelu@yahoo.com>
2024-04-19 13:31:12 +0300

    push

--- website/urls.py
+++ website/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -30,15 +31,6 @@ urlpatterns = [
 from django.urls import re_path
 from django.views.static import serve
 
-# ... the rest of your URLconf goes here ...
-
 if settings.DEBUG:
-    urlpatterns += [
-        re_path(
-            r"^media/(?P<path>.*)$",
-            serve,
-            {
-                "document_root": settings.MEDIA_ROOT,
-            },
-        ),
-    ]
+    urlpatterns.append(
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}))
