ff2ac9d121332522657a0e6916aebbbca1239319
h41pa <madalin_chelu@yahoo.com>
2024-05-19 19:27:35 +0300

    push

--- website/urls.py
+++ website/urls.py
@@ -24,9 +24,8 @@ from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('netflix.urls')),
-    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT, }, ),
-    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT, }, ),
-
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
 ]
 
 if settings.DEBUG:
